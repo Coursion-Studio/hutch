d79e37acfe874eee3641671cd87e6c68e062fad5
kuraydev <kuraydev.io@gmail.com>
2026-05-11 22:46:50 +0300

    Hutch 0.2.3

--- Casks/hutch.rb
+++ Casks/hutch.rb
@@ -1,6 +1,6 @@
 cask "hutch" do
-  version "0.2.2"
-  sha256 "0312583deb2a41dfae6ca923462ac168a5ee7456ba4055203cdf560b996b01bf"
+  version "0.2.3"
+  sha256 "b8f64eebfc7d8198f90755c4c0bda87d4660d0b47624bda68440fe677040e3c9"
 
   url "https://github.com/Coursion-Studio/hutch/releases/download/v#{version}/Hutch-#{version}.dmg",
       verified: "github.com/Coursion-Studio/hutch/"
